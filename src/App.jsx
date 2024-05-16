0995c0bacac4f29f3002826d40887f57bb6c93d4
Shiwam Shahare <com.shahare@gmail.com>
2024-05-17 03:01:44 +0530

    Project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(6)
+  const [numbers, setNumbers] = useState(false)
+  const [spChars, setSpChars] = useState(false)
+  const [password, setPassword] = useState("")
+
+  const passwordRef = useRef(null)
+
+  const passwordGenrator = useCallback(() => {
+    let pass = ""
+
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz"
+    if (numbers) str += "0123456789"
+    if (spChars) str += "`~!@#$%^&*_+-={}[]"
+    
+    for (let i = 1; i <=length; i++) {
+      let gen = Math.floor(Math.random() * str.length + 1) 
+      pass += str.charAt(gen) 
+    }
+    setPassword(pass)
+  }, [length, numbers, spChars, setPassword])
+
+  const copyPass = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password)
+  })
+  
+  useEffect(() => {
+    passwordGenrator()
+  },[length, numbers, spChars, passwordGenrator])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <div className='my-56 rounded-xl max-w-lg mx-auto px-1 py-2.5 bg-gray-600'>
+      <h1 className='text-white text-center text-3xl'>Password Genrator</h1>
+      <div className='flex justify-center items-center gap-3'>
+        <input type="text" value={password} className='rounded-md outline-none px-1 py-1' placeholder='password' readOnly ref={passwordRef}/>
+        <button className='rounded-lg bg-black mb-2 my-2 p-2.5 uppercase text-teal-500 cursor-pointer' onClick={copyPass}>copy</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='flex justify-center text-lime-400 gap-3'>
+        <div className='flex justify-center gap-x-1'>
+          <input type="range" min={6} max={32} value={length} onChange={(e) => {
+            setLength(e.target.value)
+          }}/>
+          <label>Length : { length}</label>
+        </div>
+        <div className=' flex justify-center gap-x-1'>
+          <input type="checkbox" id='numberInput' onChange={() => {
+            setNumbers((prev)=>!prev)
+          }}/>
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className=' flex justify-center gap-x-1'>
+          <input type="checkbox" id='spCharInput' onChange={() => {
+            setSpChars((prev)=>!prev)
+          }}/>
+          <label htmlFor="spCharInput">Special Characters</label>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
